72e2d1df09415e917d6a228d507db68b20ffe049
ajay-keelu <deepakdeva000@gmail.com>
2024-03-29 11:44:26 +0530

    Updated task

--- EmployeeDirectory.UI/Program.cs
+++ EmployeeDirectory.UI/Program.cs
@@ -8,14 +8,22 @@ public class Program
 
     static void Main()
     {
-        var serviceProvider = new ServiceCollection()
-        .AddSingleton<IJsonServices, JsonServices>()
-        .AddSingleton<IRoleService, RoleService>()
-        .AddSingleton<IEmployeeService, EmployeeService>()
-        .BuildServiceProvider();
+        Initialize();
+    }
+    static void Initialize()
+    {
+        ServiceProvider serviceProvider = Configure();
         IEmployeeService employeeService = serviceProvider.GetRequiredService<IEmployeeService>();
         IRoleService roleService = serviceProvider.GetRequiredService<IRoleService>();
         new EmployeeDirectory.UI.EmployeeDirectory(employeeService, roleService).Initialize();
+    }
+    static ServiceProvider Configure()
+    {
+        return new ServiceCollection()
+                .AddSingleton<IJsonServices, JsonServices>()
+                .AddSingleton<IRoleService, RoleService>()
+                .AddSingleton<IEmployeeService, EmployeeService>()
+                .BuildServiceProvider();
     }
 
 }
